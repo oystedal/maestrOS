a1733584fd4c85fa69a85ed43ce396d5b30f6aaf
Øystein Dale <oystedal@ifi.uio.no>
2017-04-14 20:10:43 +0200

    Parse the multiboot info header

--- loader/loader.c
+++ loader/loader.c
@@ -4,9 +4,18 @@
 #include "interrupts.h"
 #include "memory.h"
 
+struct mmap_entry
+{
+    uint32_t size;
+    uint32_t addr_high;
+    uint32_t addr_low;
+    uint32_t len_high;
+    uint32_t len_low;
+    uint32_t type;
 };
 
 void halt(void);
+void parse_multiboot_info(uint32_t addr);
 
 void loader_main(uint32_t magic, uint32_t addr)
 {
@@ -42,9 +51,33 @@ void loader_main(uint32_t magic, uint32_t addr)
     early_init_memory();
     enable_paging();
 
+    parse_multiboot_info(addr);
+
 end:
     kprintf("End of loader_main.\n");
     halt();
+}
+
+void
+parse_multiboot_info(uint32_t addr)
+{
+    uint32_t region = (uint32_t)map_region(addr, 0x1000);
+    multiboot_info_t *info = (void*)region;
+    kprintf("mmap addr: 0x%x\n", info->mmap_addr);
+    kprintf("mmap length: 0x%x\n", info->mmap_length);
+
+    struct mmap_entry *mmap = (void*)map_region(info->mmap_addr, 0x1000);
+    uint32_t mmap_end = (uint32_t)mmap + info->mmap_length;
+
+    for (; (uint32_t)mmap < mmap_end; mmap++) {
+        kprintf("size = 0x%x, base_addr = 0x%x:%x, length = 0x%x:%x, type = %s\n",
+                mmap->size,
+                mmap->addr_high,
+                mmap->addr_low,
+                mmap->len_high,
+                mmap->len_low,
+                mmap->type == 0x1 ? "available" : "reserved");
+    }
 }
 
 void halt(void)
