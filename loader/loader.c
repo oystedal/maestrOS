ab9458cb053568690607fef2687735f40d190ba5
Øystein Dale <oystedal@ifi.uio.no>
2017-04-15 14:48:12 +0200

    Parse ELF headers of multiboot modules

--- loader/loader.c
+++ loader/loader.c
@@ -4,6 +4,8 @@
 #include "interrupts.h"
 #include "memory.h"
 
+unsigned long load_elf64_module(uint32_t address);
+
 struct mmap_entry
 {
     uint32_t size;
@@ -63,21 +65,59 @@ parse_multiboot_info(uint32_t addr)
 {
     uint32_t region = (uint32_t)map_region(addr, 0x1000);
     multiboot_info_t *info = (void*)region;
+#if 0
     kprintf("mmap addr: 0x%x\n", info->mmap_addr);
     kprintf("mmap length: 0x%x\n", info->mmap_length);
+#endif
 
-    struct mmap_entry *mmap = (void*)map_region(info->mmap_addr, 0x1000);
-    uint32_t mmap_end = (uint32_t)mmap + info->mmap_length;
+    {
+        struct mmap_entry *mmap = (void*)map_region(info->mmap_addr, 0x1000);
+        uint32_t mmap_end = (uint32_t)mmap + info->mmap_length;
 
-    for (; (uint32_t)mmap < mmap_end; mmap = (struct mmap_entry *)((uint32_t)mmap + mmap->size + sizeof(mmap->size))) {
-        kprintf("size = 0x%x, base_addr = 0x%x:%x, length = 0x%x:%x, type = %s\n",
-                mmap->size,
-                mmap->addr_high,
-                mmap->addr_low,
-                mmap->len_high,
-                mmap->len_low,
-                mmap->type == MULTIBOOT_MEMORY_AVAILABLE ? "available" : "reserved");
+        for (; (uint32_t)mmap < mmap_end; mmap = (struct mmap_entry *)((uint32_t)mmap + mmap->size + sizeof(mmap->size))) {
+#if 0
+            kprintf("base = 0x%x:0x%x, length = 0x%x:0x%x, type = %s\n",
+                    mmap->addr_high,
+                    mmap->addr_low,
+                    mmap->len_high,
+                    mmap->len_low,
+                    mmap->type == MULTIBOOT_MEMORY_AVAILABLE ? "available" : "reserved");
+#endif
+
+            if (mmap->type == MULTIBOOT_MEMORY_AVAILABLE) {
+                if (mmap->addr_high == 0) {
+                    if (mmap->len_high) {
+                        kprintf("Ignoring region with len_high != 0\n");
+                    } else {
+                        memory_add_region(mmap->addr_low, mmap->len_low);
+                    }
+                } else {
+                    // TODO: 64-bit memory pool
+                }
+            }
+        }
+
+        unmap_region(info->mmap_addr, 0x1000);
     }
+
+    {
+        multiboot_module_t *mod = (void*)map_region(info->mods_addr, 0x1000);
+        uint32_t count = info->mods_count;
+
+        for (uint32_t i = 0; i < count; i++) {
+            kprintf("mod %d - start: 0x%x end: 0x%x\n", i, mod->mod_start, mod->mod_end);
+
+            uint32_t m = (uint32_t)map_region(mod->mod_start, 0x1000);
+            load_elf64_module(m);
+
+            mod++;
+            break;
+        }
+
+        unmap_region(info->mods_addr, 0x1000);
+    }
+
+    unmap_region(addr, 0x1000);
 }
 
 void halt(void)
